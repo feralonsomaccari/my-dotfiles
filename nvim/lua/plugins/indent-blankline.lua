1eb7a646b949c7df240665916dd48ff6ad68a656
maccarif <fernando.maccari@barrons.com>
2025-04-16 19:19:00 +0200

    feat: disable ident scope

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -2,7 +2,7 @@ return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
 	opts = {
-		scope = { show_start = false, show_end = false },
+		scope = { enabled = false },
 		indent = { char = "│" },
 	},
 }
