9b5f7970d8b007a2b9ae9fe042935e41cc124f41
maccarif <fernando.maccari@barrons.com>
2025-04-16 19:19:16 +0200

    chore: update colors for consistency

--- nvim/lua/plugins/vscode.lua
+++ nvim/lua/plugins/vscode.lua
@@ -7,7 +7,7 @@ return {
       italic_comments = true,    -- Enable italic comments
       disable_nvimtree_bg = true -- Disable nvim-tree background color
     })
- ]]
+    ]]
     vim.cmd('colorscheme gruvbox-material')
 
     local mainColor = vim.api.nvim_get_hl_by_name('Normal', true).background or "purple"
@@ -16,20 +16,31 @@ return {
     local cursorColor = vim.api.nvim_get_hl_by_name('CursorLine', true).background or "purple"
     local methodColor = vim.api.nvim_get_hl_by_name('@keyword', true).foreground or "purple"
 
+    local variableColor = vim.api.nvim_get_hl_by_name('@variable', true).foreground or "purple"
+
+
+
     -- Highlight for relative line numbers (not active line)
     vim.api.nvim_set_hl(0, 'LineNr', { fg = mainColor })
     -- Highlight for the current line number (active line)
     vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = grayColor })
 
+    -- Highlight for Floats Windows
+    vim.api.nvim_set_hl(0, 'FloatBorder', { fg = variableColor, bg = mainColor })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { fg = "NONE", bg = "NONE" })
+    -- vim.api.nvim_set_hl(0, 'FloatTitle', {fg = "NONE", bg = "NONE"})
+
     -- File name in Lualine
     vim.api.nvim_set_hl(0, "LualineFilename", { fg = methodColor, bg = "NONE" })
-    vim.api.nvim_set_hl(0, 'StatusLine', { fg = mainColor })   -- Status line
+    vim.api.nvim_set_hl(0, 'StatusLine', { fg = mainColor })
 
-    vim.api.nvim_set_hl(0, 'VertSplit', { fg = mainColor })    -- Vertical split separator
-    vim.api.nvim_set_hl(0, 'WinSeparator', { fg = mainColor }) -- Window separator
+
+    vim.api.nvim_set_hl(0, 'VertSplit', { fg = mainColor })
+    vim.api.nvim_set_hl(0, 'WinSeparator', { fg = mainColor })
 
     vim.api.nvim_set_hl(0, "Comment", { fg = grayColor })
     vim.api.nvim_set_hl(0, "@comment", { link = "Comment" })
+    vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { link = "Comment" })
 
     -- Neo-tree colors
     vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = mainColor })
