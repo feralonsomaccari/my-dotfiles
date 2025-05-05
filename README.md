2f6510f841cc8ba3fc247f68ea036eadddbd8a43
maccarif <fernando.maccari@barrons.com>
2025-05-05 16:49:43 +0300

    feat: add instalation bash script

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This Repo contains my dot files so I can take them with me everywhere.
 
+### Installing
+
+In this repo, you will find an install.sh bash script that creates the necessary symlinks for all the configuration files.
+The script will ask if you are on mobile to determine if the Termux configuration should be installed and will skip any configurations
+that are unnecessary for mobile.
+
 ### Development
 
 - IDE: Nvim
@@ -12,3 +18,9 @@ This Repo contains my dot files so I can take them with me everywhere.
 
 - News: Newsboat
 - Media: mpv and yt-dlp
+- Phone: Termux
+
+### Dependencies
+
+- Nodejs (for nvim, lsp and lint)
+- Lua (for nvim)
---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -x
+
+read -p "Are you in Mobile (y/n)? [n]: " mobile
+mobile=${mobile:-n}
+
+# INSTALL NVIM CONF
+mkdir -p ~/.config/nvim/
+ln -sf "$(realpath "$(pwd)/nvim")" ~/.config/nvim
+echo "Symlinked Nvim config"
+
+if [[ "$mobile" == "y" ]]; then
+  # INSTALL TERMUX CONF
+  mkdir -p ~/.termux/
+  ln -sf "$(realpath "$(pwd)/termux/termux.properties")" ~/.termux/termux.properties
+  echo "Symlinked termux config"
+else
+  # INSTALL ALACRITTY CONF
+  mkdir -p ~/.config/alacritty/
+  ln -sf "$(realpath "$(pwd)/alacritty")" ~/.config/alacritty
+  echo "Symlinked alacritty config"
+
+  # INSTALL NEWSBOAT CONF
+  mkdir -p ~/.newsboat/
+  ln -sf "$(realpath "$(pwd)/newsboat/config")" ~/.newsboat/config
+  ln -sf "$(realpath "$(pwd)/newsboat/urls")" ~/.newsboat/urls
+  echo "Symlinked newsboat config"
+
+  # INSTALL MPV CONF
+  mkdir -p ~/.config/mpv/
+  ln -sf "$(realpath "$(pwd)/mpv")" ~/.config/mpv
+  echo "Symlinked mpv config"
+fi
+
