07a2c139d1eeba5731155c1f8aeccdac54f17fee
maccarif <feralonsomaccari@gmail.com>
2025-05-14 23:20:21 +0300

    refactor: update alpha conf

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -7,33 +7,10 @@ return {
   config = function()
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.startify")
-
-    local cdir = vim.fn.getcwd()
-
-    local mru_section = dashboard.mru(1, cdir, 1)
     dashboard.nvim_web_devicons.enabled = false
-
     dashboard.section.mru.val = { { type = "padding", val = 0 } }
-    -- dashboard.section.buttons.val = {}
-    -- dashboard.section.mru_cwd.val = { { type = "padding", val = 1 } }
-
-
-
-    dashboard.section.header.val = {
-
-      [[          ▀████▀▄▄              ▄█ ]],
-      [[            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ]],
-      [[    ▄        █          ▀▀▀▀▄  ▄▀  ]],
-      [[   ▄▀ ▀▄      ▀▄              ▀▄▀  ]],
-      [[  ▄▀    █     █▀   ▄█▀▄      ▄█    ]],
-      [[  ▀▄     ▀▄  █     ▀██▀     ██▄█   ]],
-      [[   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ]],
-      [[    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ]],
-      [[   █   █  █      ▄▄           ▄▀   ]],
-    }
+    dashboard.section.header.val = {}
 
     alpha.setup(dashboard.opts)
-
-
   end,
 }
