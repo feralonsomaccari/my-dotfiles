c16f9804bc691a68daf32e36dd375bdd9fddf3e9
maccarif <feralonsomaccari@gmail.com>
2025-09-29 15:14:57 +0200

    feat: add leetcode pluging

---
+++ nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,13 @@
+return {
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate", -- if you have `nvim-treesitter` installed
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+lang = "go"
+    -- configuration goes here
+  },
+}
