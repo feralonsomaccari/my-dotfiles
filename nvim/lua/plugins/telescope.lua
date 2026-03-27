9c4e0ccbafc2c6a5768a42ad434dd38783115a02
maccarif <feralonsomaccari@gmail.com>
2026-03-27 12:16:50 +0100

    feat: add multigrep

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -13,6 +13,52 @@ local function focus_preview(prompt_bufnr)
   -- api.nvim_set_current_win(winid)
 end
 
+
+local live_multigrep = function(opts)
+  opts = opts or {}
+  opts.cwd = opts.cwd or vim.uv.cwd()
+
+  local pickers = require("telescope.pickers")
+  local finders = require("telescope.finders")
+  local conf = require("telescope.config").values
+
+  local make_entry = require("telescope.make_entry")
+
+  local finder = finders.new_async_job({
+    entry_maker = make_entry.gen_from_vimgrep(opts),
+    command_generator = function(prompt)
+      if not prompt or prompt == "" then
+        return nil
+      end
+
+      local pieces = vim.split(prompt, "  ")
+      local args = { "rg" }
+      if pieces[1] then
+        table.insert(args, "-e")
+        table.insert(args, pieces[1])
+      end
+      if pieces[2] then
+        table.insert(args, "-g")
+        table.insert(args, pieces[2])
+      end
+
+      return vim.tbl_flatten({
+        args,
+        { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
+      })
+    end,
+  })
+
+  pickers.new(opts, {
+    debounce = 100,
+    prompt_title = "Multi Grep",
+    finder = finder,
+    previewer = conf.grep_previewer(opts),
+    sorter = require("telescope.sorters").empty(),
+  }):find()
+end
+
+
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -100,8 +146,9 @@ return {
 
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>p", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
+      -- vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
       vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>/", live_multigrep, {})
     end,
   },
   {
